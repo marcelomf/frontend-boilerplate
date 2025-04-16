b47313e2b46c78ba65be4490fbc30c6b6529b4ce
Marcelo Fleury <marcelomf@gmail.com>
2025-04-16 13:01:27 -0300

    fix

---
+++ addshadcn.sh
@@ -0,0 +1,3 @@
+
+#!/bin/bash
+pnpm dlx shadcn@latest add accordion alert-dialog avatar badge breadcrumb button card chart checkbox collapsible command dialog dropdown-menu form input label popover progress scroll-area select separator sheet sidebar skeleton sonner switch table tabs textarea toggle-group toggle tooltip
